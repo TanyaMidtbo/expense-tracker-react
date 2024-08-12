c5e32bc8f20a15aa58fb3dd9c9d6a41e52410874
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-08-12 16:49:07 +0200

    import bootstrap, create ExpenceList

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import DisplayMessage from './DisplayMessage'
-import DisplayRandomNum from './DisplayRandomNum'
+import { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "Bread",
+      amount: 40,
+      category: "groceries",
+    },
+    {
+      id: 2,
+      description: "Monthly Ruter pass",
+      amount: 50,
+      category: "transportation",
+    },
+    { id: 3, description: "Electricity bill", amount: 60, category: "housing" },
+    { id: 4, description: "Dress", amount: 70, category: "clothes" },
+    {
+      id: 5,
+      description: "1 spotify subscription",
+      amount: 85,
+      category: "other",
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
 
   return (
     <>
-      <DisplayRandomNum />
-      <DisplayMessage />
+      <ExpenseList items={expenses} deleteItem={deleteItem} />
     </>
-  )
+  );
 }
 
-
-
-
-export default App
+export default App;
--- src/DisplayMessage.jsx
+++
@@ -1,7 +0,0 @@
-function DisplayMessage(){
-	return (
-	  <h1>The message from DisplayMessage</h1>
-	)
-  }
-
-export default DisplayMessage
